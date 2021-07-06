22f802cc347d702d736522fe5031494708225927
Renan <souza.rnn@gmail.com>
2021-07-06 17:28:35 -0300

    Product details route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
 import Home from './pages/Home';
+import ProductDetails from './pages/ProjectDetails';
 
 const Routes = () => (
   <BrowserRouter>
@@ -11,8 +12,11 @@ const Routes = () => (
       <Route path="/" exact>
         <Home />
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>
         <Catalog />
+      </Route>
+      <Route path="/products/:productId">
+        <ProductDetails />
       </Route>
       <Route path="/admin">
         <Admin />
---
+++ frontweb/src/pages/ProjectDetails/index.tsx
@@ -0,0 +1,9 @@
+
+
+const ProductDetails = () => {
+    return (
+        <h1>Página product details</h1>
+    )
+}
+
+export default ProductDetails;
