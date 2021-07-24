e2b0d402ee204ac6f1dd1903066ef99b315256cf
Renan <souza.rnn@gmail.com>
2021-07-24 20:52:15 -0300

    Products save

--- frontweb/src/pages/Admin/Login/index.tsx
+++ frontweb/src/pages/Admin/Login/index.tsx
@@ -8,7 +8,7 @@ import { saveAuthData } from '../../../util/storage';
 import { getTokenData } from '../../../util/auth';
 import './styles.css';
 
-type FormData = {
+type CredentialsDTO = {
   username: string;
   password: string;
 };
@@ -30,11 +30,11 @@ const Login = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<CredentialsDTO>();
 
   const history = useHistory();
 
-  const onSubmit = (formData: FormData) => {
+  const onSubmit = (formData: CredentialsDTO) => {
     requestBackendLogin(formData)
       .then((response) => {
         setHasError(false);
--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,35 +1,102 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { Product } from '../../../../types/product';
+import { requestBackend } from '../../../../util/requests';
 import './styles.css';
 
 const Form = () => {
+
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Product>();
+
+  const onSubmit = (formData: Product) => {
+
+    const data = { ...formData, imgUrl: 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg', categories: [ {id: 1, name: "Teste"} ]}
+
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/products',
+      withCredentials: true,
+      data,
+    };
+
+    requestBackend(config).then((response) => {
+      console.log(response.data);
+    });
+  };
+
+  const handleCancel = () => {
+    history.push("/admin/products");
+  }
+
   return (
     <div className="product-crud-container">
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row product-crud-inputs-container">
             <div className="col-lg-6 product-crud-inputs-left-container">
               <div className="margin-bottom-30">
-                <input type="text" className="form-control base-input" />
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do produto"
+                  name="name"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
               </div>
+
               <div className="margin-bottom-30">
-                <input type="text" className="form-control base-input" />
-              </div>
-              <div>
-                <input type="text" className="form-control base-input" />
+                <input
+                  {...register('price', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.price ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Preço do produto"
+                  name="price"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
               </div>
             </div>
             <div className="col-lg-6">
               <div>
                 <textarea
-                  name=""
-                  className="form-control h-auto base-input"
                   rows={10}
+                  {...register('description', {
+                    required: 'Campo obrigatório',
+                  })}
+                  className={`form-control base-input h-auto ${
+                    errors.description ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Descrição"
+                  name="description"
                 ></textarea>
+                <div className="invalid-feedback d-block">
+                  {errors.description?.message}
+                </div>
               </div>
             </div>
           </div>
           <div className="product-crud-buttons-container">
-            <button className="btn btn-outline-danger product-crud-button">
+            <button className="btn btn-outline-danger product-crud-button" onClick={handleCancel}>
               CANCELAR
             </button>
             <button className="btn btn-primary product-crud-button text-white">
