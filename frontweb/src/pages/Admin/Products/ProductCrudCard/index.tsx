42718d602674e0202d4ca9f12ca7c3b6634e0889
Renan <souza.rnn@gmail.com>
2021-07-24 13:58:03 -0300

    Finishing ProductCrudCard mobile layout

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -19,11 +19,19 @@ const ProductCrudCard = ({ product }: Props) => {
           <h6>{product.name}</h6>
           <ProductPrice price={product.price} />
         </div>
-        <div className="product-crud-category-container">
+        <div className="product-crud-categories-container">
           {product.categories.map((category) => (
             <CategoryBadge name={category.name} key={category.id} />
           ))}
         </div>
+      </div>
+      <div className="product-crud-card-buttons-container">
+        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
+          EXCLUIR
+        </button>
+        <button className="btn btn-outline-secondary product-crud-card-button">
+          EDITAR
+        </button>
       </div>
     </div>
   );
