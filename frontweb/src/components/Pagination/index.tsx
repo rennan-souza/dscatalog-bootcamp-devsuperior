af0cd1a67c23804b1d332434f7caa6b1326b1c7f
Renan <souza.rnn@gmail.com>
2021-07-27 16:47:05 -0300

    Pagination finish

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -2,21 +2,29 @@ import './styles.css';
 import { ReactComponent as ArrowIcon } from '../../assets/images/arrow.svg';
 import ReactPaginate from 'react-paginate';
 
-const Pagination = () => {
+type Props = {
+  pageCount: number;
+  range: number;
+  onChange?: (pageNumber: number) => void;
+}
+
+const Pagination = ({ pageCount, range, onChange } : Props) => {
+
   return (
     <ReactPaginate
-      pageCount={10}
-      pageRangeDisplayed={3}
+      pageCount={pageCount}
+      pageRangeDisplayed={range}
       marginPagesDisplayed={1}
       containerClassName="pagination-container"
       pageLinkClassName="pagination-item"
       breakClassName="pagination-item"
-      previousLabel={<ArrowIcon />}
-      nextLabel={<ArrowIcon />}
+      previousLabel={<div className="pagination-arrow-container"><ArrowIcon /></div>}
+      nextLabel={<div className="pagination-arrow-container"><ArrowIcon /></div>}
       previousClassName="arrow-previous"
       nextClassName="arrow-next"
       activeLinkClassName="pagination-link-active"
       disabledClassName="arrow-inactive"
+      onPageChange={(items) => (onChange) ? onChange(items.selected) : {}}
     />
   );
 };
